55e67e2a169b3a0462b38787046119971244f9e0
Wonhyuk Yang <vvghjk1234@gmail.com>
2021-06-30 07:36:15 +0000

    Support -g option to debugging
    Adding the debug section in to the kernel image, we can debug as
    if it is normal user program.

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 gcc -E -x c vmlinux.lds.S | grep -v '^#' > vmlinux.lds
-gcc -c head.S page_table.S uart.c main.c printk.c map_memory.c -fno-stack-protector -Wall -mgeneral-regs-only
+gcc -c head.S page_table.S uart.c main.c printk.c map_memory.c -fno-stack-protector -Wall -mgeneral-regs-only -g
 ld -T vmlinux.lds head.o page_table.o uart.o main.o printk.o map_memory.o -o vmlinux
 objcopy -O binary vmlinux Image
